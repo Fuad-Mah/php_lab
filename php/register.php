486cbacc1b0989f829b3b3e84066c2a341c4640d
Fuad <fuadmahmud0303@gmail.com>
2025-01-22 10:41:01 +0200

    first commit

---
+++ php/register.php
@@ -0,0 +1,29 @@
+
+<?php
+  session_start();
+      $fn=$_POST['fn'];
+      $email=$_POST['email'];
+      $pass1=$_POST['pw1'];
+      $pass2=$_POST['pw2'];
+       if(!empty($fn)&& !empty($email) &&!empty($pw1) &&!empty($pw2));{
+        if($pass1==$pass2){
+            $pass=$pass2;
+            $_SESSION['fullname']=$fn;
+            $_SESSION['email']=$email;
+            $_SESSION['password']=$pass;
+            echo"full name:".$fn;
+            echo"<br>";
+            echo"email:".$email;
+        }
+        else{
+            ?>
+            <script>
+                alert('password dont match pls try again');
+                window.location.href='../register.html';
+            </script>   
+            <?php
+        }
+       }
+
+?>
+
